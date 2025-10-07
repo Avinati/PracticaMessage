6ff39a7ec6ac7b5546eb31f742b0ce47d1dc4f2c
Ann <annacitin@gmail.com>
2025-10-07 13:52:38 +0400

    Yura soso

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import Errorpic from '/public/error.png'
+import './css/ErrorPage.css'
 
 function ErrorPage() {
     return (
         <>
-        <h1> Страница ошибки </h1>
+        <div className="main-content">
+        <h1 className="ouch">Ой!</h1>
+        <h1 className="wrong">Похоже ваш арбуз разбился!</h1>
+        <p className="dis">Описание ошибок</p>
+        <img className="err" src={Errorpic} alt="Ошибка" />
+        </div>
         </>
     )
 }
